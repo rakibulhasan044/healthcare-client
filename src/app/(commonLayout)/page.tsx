c3a1cd39a0dda3d928a23dee401caff7d1c406b1
rakibulhasan044 <rhrakib044@gmail.com>
2026-05-24 00:49:22 +0600

    landing page design done

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,17 @@
 import { Hero } from "@/components/modules/Home/Hero";
+import Specialties from "@/components/modules/Home/Specialties";
+import Steps from "@/components/modules/Home/Steps";
+import Testimonials from "@/components/modules/Home/Testimonials";
+import TopRatedDoctors from "@/components/modules/Home/TopRatedDoctor";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Specialties />
+      <TopRatedDoctors />
+      <Steps />
+      <Testimonials />
     </>
   );
 }
