0e0c45c51c7ddbffac4c16c10aa0781cdeebc349
rakibulhasan044 <rhrakib044@gmail.com>
2026-05-26 03:54:11 +0600

    route setup half

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,51 +1,137 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { jwtDecode } from "jwt-decode";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import { cookies } from "next/headers";
 
-interface userInterface {
-  id: string | null | undefined;
-  email: string;
-  role: "ADMIN" | "DOCTOR" | "PATIENT";
-  exp: number;
-  iat: number;
-}
+type UserRole = "ADMIN" | "DOCTOR" | "PATIENT";
 
-const roleBasedRoutes = {
-    "ADMIN": ['/admin/dashboard', 'admin/manage-doctor', '/admin/manage-patients'],
-    "DOCTOR": ['/doctor/dashboard'],
-    "PATIENT": ['/patient/dashboard', 'patient/appointments', '/patient/medical-records']
+type RouteConfig = {
+  exact: string[];
+  patterns: RegExp[];
+};
 
-}
+const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+];
 
-const authRoutes = ["/login", "/register", "/forgot-password"];
-export function proxy(request: NextRequest) {
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
+const commonProtectedRoutes: RouteConfig = {
+  exact: ["/my-profile", "/settings"],
+  patterns: [],
+};
 
-  const { pathname } = request.nextUrl;
+const doctorProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\/doctor/],
+};
 
-  if (!accessToken && !refreshToken) {
+const adminProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\/admin/],
+};
+
+const patientProtectedRoutes: RouteConfig = {
+  exact: [],
+  patterns: [/^\dashboard/],
+};
+
+const isAuthRoutes = (pathname: string) => {
+  return authRoutes.some((route: string) => {
+    return route === pathname;
+  });
+};
+
+const isRouteMatches = (pathname: string, routes: RouteConfig): boolean => {
+  if (routes.exact.includes(pathname)) {
+    return true;
+  }
+  return routes.patterns.some((pattern: RegExp) => pattern.test(pathname));
+};
+
+const getRouteOwner = (
+  pathname: string,
+): "ADMIN" | "DOCTOR" | "PATIENT" | "COMMON" | null => {
+  if (isRouteMatches(pathname, adminProtectedRoutes)) {
+    return "ADMIN";
+  }
+  if (isRouteMatches(pathname, doctorProtectedRoutes)) {
+    return "DOCTOR";
+  }
+  if (isRouteMatches(pathname, patientProtectedRoutes)) {
+    return "PATIENT";
+  }
+  if (isRouteMatches(pathname, commonProtectedRoutes)) {
+    return "COMMON";
+  }
+  return null;
+};
+
+const getDefaultDashboardRoute = (role: string): string => {
+  if (role === "ADMIN") {
+    return "/admin/dashboard";
+  }
+  if (role === "DOCTOR") {
+    return "/doctor/dashboard";
+  }
+  if (role === "PATIENT") {
+    return "/dashboard";
+  }
+  return "/";
+};
+
+export async function proxy(request: NextRequest) {
+  const cookieStore = await cookies();
+  const pathname = request.nextUrl.pathname;
+  const accessToken = request.cookies.get("accessToken")?.value || null;
+
+  let userRole: UserRole | null = null;
+
+  if (accessToken) {
+    const verifiedToken: string | JwtPayload = jwt.verify(
+      accessToken,
+      process.env.JWT_SECRET as Secret,
+    );
+
+    if (typeof verifiedToken === "string") {
+      cookieStore.delete("accessToken");
+      cookieStore.delete("refreshToken");
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    userRole = verifiedToken.role;
+  }
+
+  const routeOwner = getRouteOwner(pathname);
+
+  //path = /doctor/appointments => "DOCTOR"
+  //path = /my-profile => "PATIENT"
+
+  const isAuth = isAuthRoutes(pathname);
+
+  //rule-1: user is logged in and trying to access auth route
+  if (accessToken && isAuth) {
     return NextResponse.redirect(
-      new URL(`/login?redirect=${pathname}`, request.url),
+      new URL(getDefaultDashboardRoute(userRole!), request.url),
     );
   }
 
-  let user: userInterface | null = null;
-  if (accessToken) {
-    try {
-      user = jwtDecode(accessToken);
-    } catch (error) {
-      console.log("error decoding access token", error);
-      return NextResponse.redirect(
-        new URL(`/login?redirect=${pathname}`, request.url),
-      );
+  //rule-2: user is trying to access public route
+  if (routeOwner === null) {
+    return NextResponse.next();
+  }
+
+  //rule-3: user is trying to access common protected route
+  if (routeOwner === "COMMON") {
+    if (!accessToken) {
+      return NextResponse.redirect(new URL("/login", request.url));
     }
   }
 
-  
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path", "/login", "/register", "/forgot-password"],
+  matcher: ["/login", "/register", "/forgot-password", "/reset-password"],
 };
--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,13 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
+import { cookies } from "next/headers";
 import z from "zod";
+import setCookieParser from "set-cookie-parser";
 
 const loginValidationZodSchema = z.object({
   email: z.string({ error: "Invalid email address" }),
-  password: z.string().min(6, {
-    error: "password is required and must be 6 characters long"
-  }).max(30),
+  password: z
+    .string()
+    .min(6, {
+      error: "password is required and must be 6 characters long",
+    })
+    .max(30),
 });
 
 export const loginUser = async (
@@ -34,14 +39,69 @@ export const loginUser = async (
       };
     }
 
-    const res = await fetch("http://localhost:4000/api/v1/auth/login", {
+    // const res = await fetch("http://localhost:4000/api/v1/auth/login", {
+    //   method: "POST",
+    //   body: JSON.stringify(loginData),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    // // .then((res) => res.json());
+
+    // // return res;
+    // const result = await res.json();
+
+    // const cookieStore = await cookies();
+
+    // cookieStore.set("accessToken", result.data.accessToken, {
+    //   httpOnly: true,
+    //   secure: false,
+    //   sameSite: "lax",
+    // });
+
+    // cookieStore.set("refreshToken", result.data.refreshToken, {
+    //   httpOnly: true,
+    //   secure: false,
+    //   sameSite: "lax",
+    // });
+
+    // return {
+    //   success: result.success,
+    //   message: result.message,
+    //   data: {
+    //     needPasswordChange: result.data.needPasswordChange,
+    //   },
+    // };
+    const response = await fetch("http://localhost:4000/api/v1/auth/login", {
       method: "POST",
-      body: JSON.stringify(loginData),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => res.json());
-    return res;
+      body: JSON.stringify({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+    });
+
+    const cookieStore = await cookies();
+
+    const rawCookies = response.headers.getSetCookie();
+
+    const parsedCookies = setCookieParser.parse(rawCookies);
+
+    for (const cookie of parsedCookies) {
+      cookieStore.set({
+        name: cookie.name,
+        value: cookie.value,
+        httpOnly: cookie.httpOnly,
+        secure: cookie.secure,
+        sameSite: cookie.sameSite as "lax" | "strict" | "none" | undefined,
+        path: cookie.path,
+        maxAge: cookie.maxAge,
+      });
+    }
+
+    return await response.json();
   } catch (error) {
     console.log(error);
     return {
