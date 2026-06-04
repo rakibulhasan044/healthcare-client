1f3efc0b261636a9e48bdb7f7ab5205253fcc31b
rakibulhasan044 <rhrakib044@gmail.com>
2026-06-04 19:31:49 +0600

    table skeleton and selct filter compponent

--- src/components/shared/SearchFilter.tsx
+++ src/components/shared/SearchFilter.tsx
@@ -43,13 +43,15 @@ const SearchFilter = ({
   }, [debouncedValue, paramName, router, searchparams]);
   return (
     <div className="relative">
-      <Search className=" absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+      <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
       <Input
         placeholder={placeholder}
         className="pl-10"
         value={value}
         onChange={(e) => setValue(e.target.value)}
         disabled={isPending}
+        // aria-label={placeholder} // ✅ add this
+        // type="search"
       />
     </div>
   );
--- src/components/shared/SelectFilter.tsx
+++ src/components/shared/SelectFilter.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+"use client";
 
-const SelectFilter = () => {
-  return (
-    <div>SelectFilter</div>
-  )
+import { useTransition } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+interface SelectFilterProps {
+  paramName: string;
+  placeholder?: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
 }
 
-export default SelectFilter
+const SelectFilter = ({
+  paramName,
+  placeholder,
+  options,
+}: SelectFilterProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [isPending, startTransition] = useTransition();
+  const currentValue = searchParams.get(paramName) || "All";
+
+  const handleChange = (value: string | null) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (value === "All") {
+      params.delete(paramName);
+    } else if (value) {
+      params.set(paramName, value);
+    } else {
+      params.delete(paramName);
+    }
+
+    startTransition(() => {
+      router.push(`?${params.toString()}`);
+    });
+  };
+
+  return (
+    <Select
+      value={currentValue}
+      onValueChange={handleChange}
+      disabled={isPending}
+    >
+      <SelectTrigger className="w-full max-w-48">
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>{paramName}</SelectLabel>
+          {options.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SelectFilter;
